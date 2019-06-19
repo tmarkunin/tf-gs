105c06f9b21f1065755efe5e86080593148321af
tmarkunin <tmarkunin@gmail.com>
2019-06-19 14:00:54 +0300

    test

--- module1.tf
+++ module1.tf
@@ -11,31 +11,34 @@ variable "key_name" {
 ##################################################################################
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
-  region     = "us-west-1"
+  region = "us-west-1"
 }
 
 ##################################################################################
 # RESOURCES
 ##################################################################################
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
 
 resource "aws_instance" "nginx" {
-  ami           = "ami-c58c1dd3"
-  instance_type = "t2.nano"
+  ami           = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
   key_name        = "${var.key_name}"
 
-  connection {
-    user        = "ec2-user"
-    private_key = "${file(var.private_key_path)}"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum install nginx -y",
-      "sudo service nginx start"
-    ]
-  }
 }
 
 ##################################################################################
