1db4020487557cfb5a4c47c665885ca624422976
tmarkunin <tmarkunin@gmail.com>
2019-06-19 16:21:28 +0300

    fixes

--- module1.tf
+++ module1.tf
@@ -126,7 +126,7 @@ resource "aws_instance" "nginx1" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo yum install nginx -y",
+      "sudo amazon-linux-extras install -y nginx1.12",
       "sudo service nginx start",
       "echo '<html><head><title>Blue Team Server</title></head><body style=\"background-color:#1F778D\"><p style=\"text-align: center;\"><span style=\"color:#FFFFFF;\"><span style=\"font-size:28px;\">Blue Team</span></span></p></body></html>' | sudo tee /usr/share/nginx/html/index.html"
     ]
