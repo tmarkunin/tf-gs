44342b424ab4d1f74ac43084f70e69a5d016162f
tmarkunin <tmarkunin@gmail.com>
2019-06-19 13:55:58 +0300

    init

---
+++ module1.tf
@@ -0,0 +1,47 @@
+
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "private_key_path" {}
+variable "key_name" {
+  default = "ec2-user"
+}
+
+##################################################################################
+# PROVIDERS
+##################################################################################
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "us-west-1"
+}
+
+##################################################################################
+# RESOURCES
+##################################################################################
+
+resource "aws_instance" "nginx" {
+  ami           = "ami-c58c1dd3"
+  instance_type = "t2.nano"
+  key_name        = "${var.key_name}"
+
+  connection {
+    user        = "ec2-user"
+    private_key = "${file(var.private_key_path)}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum install nginx -y",
+      "sudo service nginx start"
+    ]
+  }
+}
+
+##################################################################################
+# OUTPUT
+##################################################################################
+
+output "aws_instance_public_dns" {
+    value = "${aws_instance.nginx.public_dns}"
+}
